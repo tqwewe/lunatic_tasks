9be6f8323a0168d5fb55e52a063ee5188ab6935d
Ari Seyhun <ariseyhun@live.com.au>
2022-11-07 00:49:20 +0800

    docs: add docs

--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,22 @@
+//! Utilities for working with tasks in Lunatic.
+//!
+//! # Example
+//!
+//! ```
+//! let mut tasks = (0..5).rev().tasks_unordered(3, |num| {
+//!     lunatic::sleep(Duration::from_millis(num as u64 * 200));
+//!     num
+//! });
+//! assert_eq!(tasks.next(), Some(2));
+//! assert_eq!(tasks.next(), Some(3));
+//! assert_eq!(tasks.next(), Some(0));
+//! assert_eq!(tasks.next(), Some(1));
+//! assert_eq!(tasks.next(), Some(4));
+//! assert_eq!(tasks.next(), None);
+//! ```
+
+#![warn(missing_docs)]
+
 mod ordered;
 mod unordered;
 
@@ -13,13 +32,24 @@ where
     M: 'static,
 {
     /// Execute tasks buffered, with results returned in order.
+    ///
+    /// # Example
+    ///
+    /// ```
+    /// use lunatic_tasks::TaskExt;
+    ///
+    /// (0..10).tasks_ordered(2, |n| {
+    ///     lunatic::sleep(Duration::from_millis(n as u64 * 200));
+    ///     n
+    /// })
+    /// ```
     fn tasks_ordered(self, n: usize, f: fn(C) -> M) -> TasksOrdered<Self, C, M, Bincode> {
         self.tasks_ordered_with_serializer(n, f)
     }
 
     /// Execute tasks buffered, with results returned in order.
     ///
-    /// This method differs from [`tasks_ordered`] in that you may provide a different `Serializer` than `Bincode`.
+    /// This method differs from [`TaskExt::tasks_ordered`] in that you may provide a different `Serializer` than `Bincode`.
     fn tasks_ordered_with_serializer<S>(
         self,
         n: usize,
@@ -27,13 +57,30 @@ where
     ) -> TasksOrdered<Self, C, M, S>;
 
     /// Execute tasks buffered, with results returned in any order as soon as results are available.
+    ///
+    /// # Example
+    ///
+    /// ```
+    /// use lunatic_tasks::TaskExt;
+    ///
+    /// let mut tasks = (0..5).rev().tasks_unordered(3, |num| {
+    ///     lunatic::sleep(Duration::from_millis(num as u64 * 200));
+    ///     num
+    /// });
+    /// assert_eq!(tasks.next(), Some(2));
+    /// assert_eq!(tasks.next(), Some(3));
+    /// assert_eq!(tasks.next(), Some(0));
+    /// assert_eq!(tasks.next(), Some(1));
+    /// assert_eq!(tasks.next(), Some(4));
+    /// assert_eq!(tasks.next(), None);
+    /// ```
     fn tasks_unordered(self, n: usize, f: fn(C) -> M) -> TasksUnordered<Self, C, M, Bincode> {
         self.tasks_unordered_with_serializer(n, f)
     }
 
     /// Execute tasks buffered, with results returned in any order as soon as results are available.
     ///
-    /// This method differs from [`tasks_unordered`] in that you may provide a different `Serializer` than `Bincode`.
+    /// This method differs from [`TaskExt::tasks_unordered`] in that you may provide a different `Serializer` than `Bincode`.
     fn tasks_unordered_with_serializer<S>(
         self,
         n: usize,
