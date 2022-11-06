f6753b8af2192dced9b56f29b0f5cdc48b0f5741
Ari Seyhun <ariseyhun@live.com.au>
2022-11-07 00:50:32 +0800

    chore: update examples

--- examples/ordered.rs
+++ examples/ordered.rs
@@ -3,13 +3,11 @@ use std::time::Duration;
 use lunatic_tasks::TaskExt;
 
 fn main() {
-    let tasks = [5, 5, 2, 2, 3, 3, 4, 4, 5, 5]
-        .into_iter()
-        .tasks_ordered(3, |num| {
-            lunatic::sleep(Duration::from_millis(num as u64 * 200));
-            println!("{num}");
-            num
-        });
+    let tasks = (0..10).tasks_ordered(3, |num| {
+        lunatic::sleep(Duration::from_millis(num as u64 * 200));
+        println!("{num}");
+        num
+    });
     for result in tasks {
         println!("got result {result}");
     }
--- examples/unordered.rs
+++ examples/unordered.rs
@@ -3,14 +3,14 @@ use std::time::Duration;
 use lunatic_tasks::TaskExt;
 
 fn main() {
-    let tasks = [5, 5, 2, 2, 3, 3, 4, 4, 5, 5]
-        .into_iter()
-        .tasks_unordered(3, |num| {
-            lunatic::sleep(Duration::from_millis(num as u64 * 200));
-            println!("{num}");
-            num
-        });
-    for result in tasks {
-        println!("got result {result}");
-    }
+    let mut tasks = (0..5).rev().tasks_unordered(3, |num| {
+        lunatic::sleep(Duration::from_millis(num as u64 * 200));
+        num
+    });
+    assert_eq!(tasks.next(), Some(2));
+    assert_eq!(tasks.next(), Some(3));
+    assert_eq!(tasks.next(), Some(0));
+    assert_eq!(tasks.next(), Some(1));
+    assert_eq!(tasks.next(), Some(4));
+    assert_eq!(tasks.next(), None);
 }
